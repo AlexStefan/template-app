283f53276b168c97648f6836edb1b8e38a1289ef
Alexandru.Stefan <alex.steefan@gmail.com>
2018-02-28 14:22:11 +0200

    Played around Linking and targeted CPU Arhitecture to reduce apk size

---
+++ Internationalization/Internationalization.Droid/LinkerPleaseInclude.cs
@@ -0,0 +1,20 @@
+﻿using MvvmCross.IoC;
+using System.ComponentModel;
+
+namespace Internationalization.Droid
+{
+    public class LinkerPleaseInclude
+    {
+        public void Include(MvxPropertyInjector injector)
+        {
+            injector = new MvxPropertyInjector();
+        }
+
+        public void Include(INotifyPropertyChanged changed)
+        {
+            changed.PropertyChanged += (sender, e) => {
+                var test = e.PropertyName;
+            };
+        }
+    }
+}
